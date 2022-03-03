18047c31eb85c12ec680628aeff848acf0719c08
Mehdi BENNOUNA <bennouna.mehdi@hotmail.com>
2022-03-03 16:44:42 +0100

    Outlined eventUpdate behavior

--- src/events/eventUpdate.ts
+++ src/events/eventUpdate.ts
@@ -1,5 +1,21 @@
+import { client } from "..";
 import { Event } from "../structures/Event";
 
-export default new Event("guildScheduledEventUpdate", () => {
-    console.log("updated event");
+export default new Event("guildScheduledEventUpdate", (oldEvent, event) => {
+    if (!client.trackedEvents.get(oldEvent.name)) return;
+
+    if (event.status === "ACTIVE") {
+        //add it to the active trackedEvents list
+        console.log("Active");
+    }
+
+    if (event.status === "COMPLETED") {
+        //should return the csv here
+        console.log("Completed");
+    }
+
+    if (event.status === "CANCELED") {
+        //should remove it from the tracked list and do nothin
+        console.log("Canceled");
+    }
 });
