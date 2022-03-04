84b83d033f5d1b3b93cf9ea2e05dcea306c4ff0d
Mehdi BENNOUNA <bennouna.mehdi@hotmail.com>
2022-03-04 23:30:23 +0100

    active handling of activeEvent update

--- src/events/eventUpdate.ts
+++ src/events/eventUpdate.ts
@@ -1,20 +1,24 @@
+import { Collection } from "discord.js";
 import { client } from "..";
 import { Event } from "../structures/Event";
 
-export default new Event("guildScheduledEventUpdate", (oldEvent, event) => {
-    if (!client.trackedEvents.get(oldEvent.name)) return;
+export default new Event("guildScheduledEventUpdate", (oldEvent, newEvent) => {
+    if (!client.trackedEvents.get(oldEvent.id)) return;
 
-    if (event.status === "ACTIVE") {
-        //add it to the active trackedEvents list
+    if (newEvent.status === "ACTIVE") {
+        client.activeTrackedEvents.set(newEvent.id, {
+            event: newEvent,
+            attendees: new Collection(),
+        });
         console.log("Active");
     }
 
-    if (event.status === "COMPLETED") {
+    if (newEvent.status === "COMPLETED") {
         //should return the csv here
         console.log("Completed");
     }
 
-    if (event.status === "CANCELED") {
+    if (newEvent.status === "CANCELED") {
         //should remove it from the tracked list and do nothin
         console.log("Canceled");
     }
