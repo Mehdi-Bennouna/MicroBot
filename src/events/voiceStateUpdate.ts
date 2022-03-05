8be3f7add0a749f931eb197d12fd47ec5864bfdb
Mehdi BENNOUNA <bennouna.mehdi@hotmail.com>
2022-03-05 16:05:12 +0100

    Now tracks every attendee time in an event

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,8 +1,60 @@
 import { client } from "..";
 import { Event } from "../structures/Event";
 
-export default new Event("voiceStateUpdate", async (oldState, newState) => {});
 
-const isTrackedChannel = (id: string) => {
-    return client.activeTrackedEvents.find((event) => event.event.channel.id === id);
+export default new Event("voiceStateUpdate", async (oldState, newState) => {
+    //ignoring non event stuff
+    if (oldState.channelId === newState.channelId) {
+        return;
+    }
+
+    if (!channelEvent(oldState.channelId) && !channelEvent(newState.channelId)) {
+        return;
+    }
+
+    const currentTime = new Date().getTime();
+    const userId = oldState.member.id;
+
+    let joinTime: number;
+    let totalTime: number;
+
+    if (channelEvent(oldState.channelId)) {
+        const oldChannelEvent = channelEvent(oldState.channelId).event;
+        const oldChannelActiveEvent = client.activeTrackedEvents.get(oldChannelEvent.id);
+
+        console.log("<Voice Update> Left an event channel");
+
+        joinTime = oldChannelActiveEvent.attendees.get(userId).joinTime;
+
+        totalTime =
+            oldChannelActiveEvent.attendees.get(userId).totalTime +
+            (currentTime - joinTime);
+
+        oldChannelActiveEvent.attendees.set(userId, {
+            joinTime: joinTime,
+            totalTime: totalTime,
+        });
+
+        console.log(`total : ${totalTime / 1000}`);
+    }
+
+    if (channelEvent(newState.channelId)) {
+        const newChannelEvent = channelEvent(newState.channelId).event;
+        const newChannelActiveEvent = client.activeTrackedEvents.get(newChannelEvent.id);
+
+        console.log("<Voice Update> Joined an event channel");
+
+        joinTime = currentTime;
+
+        totalTime = newChannelActiveEvent.attendees?.get(userId)?.totalTime | 0;
+
+        newChannelActiveEvent.attendees.set(userId, {
+            joinTime: joinTime,
+            totalTime: totalTime,
+        });
+    }
+});
+
+const channelEvent = (id: string) => {
+    return client.activeTrackedEvents.find((event) => event.event.channelId === id);
 };
