22234bca04afecfb31cae845ad8013ae3c35e1e0
Mehdi BENNOUNA <bennouna.mehdi@hotmail.com>
2022-03-06 00:21:58 +0100

    now returning proper username and total time after event completion

--- src/events/eventUpdate.ts
+++ src/events/eventUpdate.ts
@@ -1,8 +1,8 @@
-import { Collection } from "discord.js";
+import { Collection, GuildMember, GuildMemberEditData } from "discord.js";
 import { client } from "..";
 import { Event } from "../structures/Event";
 
-export default new Event("guildScheduledEventUpdate", (oldEvent, newEvent) => {
+export default new Event("guildScheduledEventUpdate", async (oldEvent, newEvent) => {
     if (!client.trackedEvents.get(oldEvent.id)) return;
 
     if (newEvent.status === "ACTIVE") {
@@ -15,12 +15,16 @@ export default new Event("guildScheduledEventUpdate", (oldEvent, newEvent) => {
 
     if (newEvent.status === "COMPLETED") {
         //should return the csv here
-        console.log("Completed");
+        const attendees = client.activeTrackedEvents.get(newEvent.id).attendees;
+        const final = attendees.map((id) => {
+            return { username: id.username, Time: id.totalTime };
+        });
+        console.log(final);
     }
 
     if (newEvent.status === "CANCELED") {
-        client.trackedEvents.delete(oldEvent.id);
         //should remove it from the tracked list and do nothin
+        client.trackedEvents.delete(oldEvent.id);
         console.log("Canceled");
     }
 });
