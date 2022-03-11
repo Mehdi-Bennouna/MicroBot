42ff8228a8366b00761971feba12229cb9d1b6b1
Mehdi BENNOUNA <Bennouna.Mehdi@hotmail.com>
2022-03-11 23:40:45 +0100

    setting up channel tracking

---
+++ src/commands/channel_tracking/track_channel.ts
@@ -0,0 +1,7 @@
+import { Command } from "../../structures/Command";
+
+export default new Command({
+    name: "track channel",
+    description: "sets a channel to tracked",
+    run: async () => {},
+});
