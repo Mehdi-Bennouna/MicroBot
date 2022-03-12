d9561a67e7ba351fcbb8dcd5484012b69df6b497
Mehdi BENNOUNA <Bennouna.Mehdi@hotmail.com>
2022-03-12 02:47:45 +0100

    fixed time tracking for people still in the channel

--- src/commands/channel_tracking/untrack_channel.ts
+++ src/commands/channel_tracking/untrack_channel.ts
@@ -6,6 +6,14 @@ import { ExtendedChannel } from "../../typings/Channel";
 export default new Command({
     name: "untrack_channel",
     description: "remove a channel from tracked and get report",
+    options: [
+        {
+            name: "channel",
+            description: "channel to untrack",
+            type: "CHANNEL",
+            required: true,
+        },
+    ],
     run: async ({ interaction }) => {
         const channel = await (
             interaction.options.getChannel("channel") as GuildChannel as ExtendedChannel
@@ -15,6 +23,22 @@ export default new Command({
             interaction.reply({ ephemeral: true, content: "Channel not tracked yet" });
             return;
         }
+
+        const currentTime = new Date().getTime();
+
+        channel.members.forEach((member) => {
+            const temp = client.trackedChannels
+                .get(channel.id)
+                .trackedMembers.get(member.id);
+
+            temp.channelTime += currentTime - temp.channelJoinTime;
+        });
+
+        console.table(
+            channel.trackedMembers.map((x) => {
+                return { username: x.user.username, Time: x.channelTime / 1000 };
+            }),
+        );
 
         client.trackedChannels.delete(channel.id);
 
