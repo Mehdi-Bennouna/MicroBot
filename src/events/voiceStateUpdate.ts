db28d01006420e617f0a2ca0576cfaf2ce5567be
Mehdi BENNOUNA <Bennouna.Mehdi@hotmail.com>
2022-03-12 02:27:04 +0100

    handling voice updates with tracked channels

---
+++ src/events/voiceStateUpdate.ts
@@ -0,0 +1,68 @@
+import { client } from "..";
+import { Event } from "../structures/Event";
+import { isTrackedChannel } from "../utils/functions";
+import { ExtendedMember } from "../typings/Member";
+
+export default new Event("voiceStateUpdate", async (oldState, newState) => {
+    //ignoring mute unmute stuff
+    if (oldState.channelId === newState.channelId) {
+        return;
+    }
+
+    const currentTime = new Date().getTime();
+
+    //joining tracked channel
+    if (isTrackedChannel(newState.channelId)) {
+        console.log(
+            `<voice-update> ${newState.member.user.username} joined '${newState.channel.name}'`,
+        );
+        const channel = client.trackedChannels.get(newState.channelId);
+        const tempMember = newState.member as ExtendedMember;
+
+        tempMember.channelTime =
+            channel.trackedMembers.get(newState.member.id)?.channelTime | 0;
+        tempMember.channelJoinTime = currentTime;
+
+        channel.trackedMembers.set(newState.member.id, tempMember);
+
+        console.table(
+            channel.trackedMembers.map((x) => {
+                return {
+                    username: x.user.username,
+                    join_time: x.channelJoinTime,
+                    total_time: x.channelTime / 1000,
+                };
+            }),
+        );
+    }
+
+    //leaveing tracked channel
+    if (isTrackedChannel(oldState.channelId)) {
+        console.log(
+            `<voice-update> ${oldState.member.user.username} left '${oldState.channel.name}'`,
+        );
+
+        const channel = client.trackedChannels.get(oldState.channelId);
+        const tempMember = oldState.member as ExtendedMember;
+
+        tempMember.channelJoinTime = channel.trackedMembers.get(
+            oldState.member.id,
+        ).channelJoinTime;
+
+        tempMember.channelTime =
+            channel.trackedMembers.get(oldState.member.id).channelTime +
+            (currentTime - tempMember.channelJoinTime);
+
+        channel.trackedMembers.set(newState.member.id, tempMember);
+
+        console.table(
+            channel.trackedMembers.map((x) => {
+                return {
+                    username: x.user.username,
+                    join_time: x.channelJoinTime,
+                    total_time: x.channelTime / 1000,
+                };
+            }),
+        );
+    }
+});
