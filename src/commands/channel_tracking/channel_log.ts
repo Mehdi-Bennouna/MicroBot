cdb121739b08b10ebb8bae0af5b50181d34fcff0
Mehdi BENNOUNA <Bennouna.Mehdi@hotmail.com>
2022-03-12 11:38:33 +0100

    added channel log command

---
+++ src/commands/channel_tracking/channel_log.ts
@@ -0,0 +1,31 @@
+import { GuildChannel } from "discord.js";
+import { Command } from "../../structures/Command";
+import { ExtendedChannel } from "../../typings/Channel";
+
+export default new Command({
+    name: "log_channel",
+    description: "console logs a channel",
+    options: [
+        {
+            name: "channel",
+            description: "channel to log",
+            type: "CHANNEL",
+            required: true,
+        },
+    ],
+    run: async ({ interaction }) => {
+        const channel = await (
+            interaction.options.getChannel("channel") as GuildChannel as ExtendedChannel
+        ).fetch();
+
+        console.table(
+            channel.trackedMembers.map((x) => {
+                return {
+                    username: x.user.username,
+                    joinTime: x.channelJoinTime,
+                    TotalTime: x.channelTime,
+                };
+            }),
+        );
+    },
+});
